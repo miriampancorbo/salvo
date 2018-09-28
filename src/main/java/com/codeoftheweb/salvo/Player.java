ceb17dfa79b3b53dd4b9805967df4315c846e78a
Miriam Pancorbo <miriam.pancorbonate@gmail.com>
2018-09-27 22:16:23 -0300

    First commit

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,48 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String userName;
+
+    public Player(){ }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,24 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			repository.save(new Player("Jack Bauer"));
+			repository.save(new Player("Chloe O'Brian"));
+			repository.save(new Player("Kim Bauer"));
+			repository.save(new Player("Tony Almeida"));
+		};
+	}
+}
